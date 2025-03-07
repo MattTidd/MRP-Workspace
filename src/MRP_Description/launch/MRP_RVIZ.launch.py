3014ff21ba67e09b501f5e1e15fdffba07658cc5
MattTidd <mtidd2@unb.ca>
2025-03-07 15:46:35 -0400

    fixed issue with joint state publisher not working

--- src/MRP_Description/launch/MRP_RVIZ.launch.py
+++ src/MRP_Description/launch/MRP_RVIZ.launch.py
@@ -19,47 +19,48 @@ from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessStart
 
 def generate_launch_description():
-    # launch arguments:
+    # launch configuration:
     use_sim_time = LaunchConfiguration('use_sim_time')
+
+    # launch arguments:
+    use_sim_time_arg = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='false',
+        description='Use sim time if true'
+    )
+
+    # paths:
     pkg_path = os.path.join(get_package_share_directory('MRP_Description'))
     rviz_config_path = os.path.join(pkg_path, 'config', 'RVIZ_config.rviz')
 
     # nodes:
     robot_state_publisher = IncludeLaunchDescription(PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('MRP_Description'),'launch','rsp.launch.py'
-                )])
-                , launch_arguments={'use_sim_time': use_sim_time, 'use_ros2_control': 'false'}.items()
+                )]), launch_arguments={'use_sim_time': use_sim_time, 'use_ros2_control': 'false'}.items()
+    )
+
+    joint_state_publisher = Node(
+        package = 'joint_state_publisher_gui',
+        executable = 'joint_state_publisher_gui',
+        parameters = [{'use_sim_time': use_sim_time}]
     )
 
     rviz = Node(
         package = 'rviz2',
         executable = 'rviz2',
-        name = 'RVIZ2',
+        name = 'rviz2',
         output = 'screen',
         arguments = ['-d', rviz_config_path],
         parameters = [{'use_sim_time' : use_sim_time}]
     )
 
-    joint_state_publisher_gui = Node(
-        package = 'joint_state_publisher_gui',
-        executable = 'joint_state_publisher_gui',
-        name = 'joint_state_publisher_gui',
-        parameters = [{'use_sim_time': use_sim_time}]
-    )
-
     return LaunchDescription([
-
-        DeclareLaunchArgument(
-            name = 'use_sim_time',
-            default_value = 'true',
-            description = 'Use simulation time'
-        ),
-
+        use_sim_time_arg,
         robot_state_publisher,
-        rviz,
-        joint_state_publisher_gui
-    
+        joint_state_publisher,
+        rviz
     ])
