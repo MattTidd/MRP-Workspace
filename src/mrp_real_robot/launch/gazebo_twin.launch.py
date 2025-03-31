5744adf05645d2df9c04d03fb81477631389e498
MattTidd <mtidd2@unb.ca>
2025-03-31 15:54:37 -0300

    created a launch file for twinning. simply launches gazebo, and spawns the robot based off of a local URDF

--- src/mrp_real_robot/launch/gazebo_twin.launch.py
+++ src/mrp_real_robot/launch/gazebo_twin.launch.py
@@ -17,16 +17,35 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, Command
+from launch_ros.parameter_descriptions import ParameterValue
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    
+    # prepare local URDF:
+    pkg_path = os.path.join(get_package_share_directory('mrp_real_robot'))
+    xacro_file = os.path.join(pkg_path, 'urdf', 'MRP_Mirror.urdf.xacro')
+    robot_description_config = Command(['xacro ', xacro_file])
+    params = {'robot_description': ParameterValue(robot_description_config, value_type = str)}
+
+    # mirrored robot state publisher:
+    mirrored_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name = 'mirrored_robot_state_publisher',
+        output='screen',
+        parameters=[params],
+        remappings = [('/robot_description', '/mirrored_robot_description')]
+    )
+
     # launch gazebo:
     gazebo_node = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
             get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]), 
-            launch_arguments = {'verbose': 'true'}.items()
+            launch_arguments = {
+                # 'verbose': 'true',
+                'extra_gazebo_args' : '--ros-args -p use_sim_time:=false'
+                }.items()
     )
 
     # spawn the robot model:
@@ -34,14 +53,14 @@ def generate_launch_description():
         package = 'gazebo_ros',
         executable = 'spawn_entity.py',
         arguments = [
-            '-topic', '/robot_description',
+            '-topic', '/mirrored_robot_description',
             '-entity', 'mirrored_robot'
         ]
     )
 
-
     return LaunchDescription([
+        mirrored_robot_state_publisher, 
         gazebo_node, 
-        spawn_entity
+        spawn_entity,
     ])
     
